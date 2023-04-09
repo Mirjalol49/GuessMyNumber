6b8016605abf99b8e21eb8c8b45e9ed4b0203c41
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2023-04-09 02:24:37 -0700

    Fully finished

--- js/app.js
+++ js/app.js
@@ -1,27 +1,60 @@
+//!GET ELEMENTS FROM DOM
 const elForm = document.querySelector("#js-form");
-let elInput = document.querySelector("#js-input");
-const elBtn = document.querySelector("#js-btn");
-const elRefresh = document.querySelector("#js-refresh")
-const elResult = document.querySelector("#js-result");
-const elRound = document.querySelector(".round");
+let elInput = elForm.querySelector("#js-input");
+const elBtn = elForm.querySelector("#js-btn");
+const elRefresh = elForm.querySelector("#js-refresh")
+const elResult = elForm.querySelector("#js-result");
+let life = document.querySelector(".life");
+let remainLife = document.querySelector(".remainlife");
+const heartImg = document.querySelector(".heart");
+const question = document.querySelector(".question");
+const mainTitle = document.querySelector(".main-title");
+const subtitle = document.querySelector(".subtitle");
+let remainlifeNumber = 5;
 let magicNumber = Math.trunc(Math.random() * 20);
 
+//! PREVENTING THE FORM FROM BEING RELOADED
 elForm.addEventListener("submit", function(event) {
   event.preventDefault();
+  
+  
+  //! CONVERTING STRING INTO INTEGER NUMBER
   inputValue = parseInt(elInput.value.trim());
   console.log(magicNumber);
-
+  
+  //! THIS FUNCTIONS DISPLAYS THE FUNCTION
   const displayMessage = function(message) {
     elResult.textContent = message;
   }
   
+  //! GAME LOGIC GOES HERE
   if (inputValue === magicNumber) {
-    displayMessage(`Congratulations🎊`)
+    displayMessage(`Vanihoyat Topdingiz, Tabriklayman, Tabassum qiling🤩🎊`)
     elInput.disabled = true;
     elRefresh.classList.add("on");
     elBtn.classList.add("refresh")
-} else {
-   displayMessage(inputValue > magicNumber ? "Too high🔥" : "Too low❄️")
-   elInput.value = "";
+  } else {
+    displayMessage(inputValue > magicNumber ? "Vaybo', muncha katta🙄" : "Bundan kattaroq raqam o'ylaganman🥱")
+    elInput.value = "";
+    remainlifeNumber--;
+  };
+  
+  life.textContent = remainlifeNumber;
+  
+  if(remainlifeNumber <= 0) {
+    life.textContent = "Afsuski yutqazdingiz, qayta urinib ko'ring...🐈‍⬛";
+    elInput.classList.add("refresh")
+    elRefresh.classList.add("on");
+    elRefresh.style.marginBottom = "0 !important";
+    elForm.classList.add("form-control")
+    displayMessage.disabled = true;
+    elResult.classList.add("refresh")
+    heartImg.classList.add("refresh");
+    question.classList.add("refresh")
+    elBtn.classList.add("refresh");
+    mainTitle.classList.add("refresh");
+    subtitle.classList.add("refresh")
   }
 });
+
+
